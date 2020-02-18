242743148acf75cfacb7c7c564e879a66ddb7e06
Mixchanawee <pat_mixky@hotmail.com>
2020-02-18 17:01:12 +0000

    change savecsv

--- savecsvfromgithub.py
+++ savecsvfromgithub.py
@@ -1,5 +1,5 @@
 import pandas as pd
 import csv
-url="https://github.com/123mix123/Mixchanawee/blob/master/videouploadedin2m.csv"
+url="https://raw.githubusercontent.com/123mix123/Mixchanawee/master/videouploadedin2m.csv"
 df = pd.read_csv(url,error_bad_lines=False)
 df.to_csv('urlcsv'+'.csv',line_terminator="\n",index=False)
